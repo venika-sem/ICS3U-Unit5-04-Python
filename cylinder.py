e7b82b842d0d74920b2d6f60210544dfa8e381b5
Venika Sem <venika.sem@mths.ca>
2022-12-06 16:22:04 +0000

    done

---
+++ cylinder.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python3
+
+# Created by : Venika Sem
+# Created on : Dec 2022
+# This program finds cylinder volume
+
+
+import math
+
+
+def cylinder_volume_calculation(radius, height):
+    # This function finds the volume of the cylinder
+
+    volume = math.pi * radius**2 * height
+
+    return volume
+
+
+def main():
+    # This function gets the radius and height from the user
+
+    radius_from_user = input("Enter the radius of the cylinder (cm): ")
+    height_from_user = input("Enter the height of the cylinder (cm): ")
+
+    try:
+        radius_from_user = int(radius_from_user)
+        height_from_user = int(height_from_user)
+        # Call function
+        final_volume = cylinder_volume_calculation(radius_from_user, height_from_user)
+        print(
+            "\nThe volume of a cylinder with the radius of {0} cm and the height of {1} cm is {2} cm³.".format(
+                radius_from_user, height_from_user, final_volume
+            )
+        )
+    except Exception:
+        print("\nInvalid Input.")
+
+    print("\nDone.")
+
+
+if __name__ == "__main__":
+    main()
